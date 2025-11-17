c5d6a4c158e4780a82d01eed2e44d604ee8d3cb1
Emir <muhammedemir.akguel@gmail.com>
2025-11-17 22:54:37 +0100

    Adding first images for Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-
+import asyncio
+from player import Player
 
 screen_width = 1280
 screen_height = 720
@@ -12,23 +13,35 @@ class Shooter_Game:
         self.clock  = pygame.time.Clock()
         self.FPS = 60
         self.gameState = True
+        self.all_sprites = pygame.sprite.Group()
+        self.player = Player((600, 400), self.all_sprites)
         
-    def runGame(self):
+
+
+    async def runGame(self):
         while self.gameState:
+            delta_t = self.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     print("LÖSCHEN")
                     self.gameState = False
                
             print("Hallo")
-            self.screen.fill((250, 240, 230))
-            self.clock.tick(self.FPS)
-            pygame.display.update()
-        
+            self.screen.fill((0,200,0))
+            
+            self.all_sprites.update(delta_t)
+            self.all_sprites.draw(self.screen)
+
+            #self.clock.tick(self.FPS)
+            pygame.display.flip()
+            await asyncio.sleep(0)
+
         pygame.quit()
         
-game = Shooter_Game()
 
-game.runGame()
-            
-        
+async def main():
+    game = Shooter_Game()
+    await game.runGame()
+
+if __name__ == "__main__":
+    asyncio.run(main())
